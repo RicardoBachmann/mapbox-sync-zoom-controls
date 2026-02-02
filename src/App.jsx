3ee96f4fd80048b60e948f72c6695d9eeec91763
Ricardo Bachmann <ricardo.bachmann@icloud.com>
2026-02-02 09:50:15 +0100

    feat: add move event listener to display real-time zoom level

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useRef, useEffect } from "react";
+import { useRef, useEffect, useState } from "react";
 import mapboxgl from "mapbox-gl";
 
 import "mapbox-gl/dist/mapbox-gl.css";
@@ -8,14 +8,24 @@ function App() {
   const mapRef = useRef(); // map instance (lifecycle)
   const mapContainerRef = useRef(); // containers html el to create the map
   const accessToken = import.meta.env.VITE_MAPBOX_TOKEN;
+  const initialCenter = [-60.17795, -6.82434];
+  const initialZoom = 5;
+  const [zoom, setZoom] = useState(initialZoom);
 
   useEffect(() => {
     mapboxgl.accessToken = accessToken;
+
     mapRef.current = new mapboxgl.Map({
       container: mapContainerRef.current,
-      center: [-60.17795, -6.82434],
-      zoom: 5,
+      center: initialCenter,
+      zoom: initialZoom,
       style: "mapbox://styles/mapbox/satellite-v9",
+    });
+
+    mapRef.current.on("move", () => {
+      const mapZoom = mapRef.current.getZoom();
+
+      setZoom(mapZoom);
     });
 
     return () => {
@@ -25,6 +35,7 @@ function App() {
 
   return (
     <>
+      <div id="zoom-bar"> Zoom:{zoom.toFixed(2)}</div>
       <div id="map-container" ref={mapContainerRef}></div>
     </>
   );
