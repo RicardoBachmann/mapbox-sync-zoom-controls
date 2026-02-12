8c0c247de2add0218bf2fa1edf1d6efb9146731f
Ricardo Bachmann <ricardo.bachmann@icloud.com>
2026-02-12 21:03:26 +0100

    refactor: reduce to 3 maps with horizontal layout

--- src/App.jsx
+++ src/App.jsx
@@ -10,16 +10,15 @@ function App() {
   const mapRefA = useRef(); // map instance (lifecycle)
   const mapRefB = useRef();
   const mapRefC = useRef();
-  const mapRefD = useRef();
+
   const mapContainerRefA = useRef(); // containers html el to create the map
   const mapContainerRefB = useRef();
   const mapContainerRefC = useRef();
-  const mapContainerRefD = useRef();
+  /*const mapContainerRefD = useRef();*/
 
   const [activeLayerA, setActiveLayerA] = useState(true);
   const [activeLayerB, setActiveLayerB] = useState(true);
   const [activeLayerC, setActiveLayerC] = useState(true);
-  const [activeLayerD, setActiveLayerD] = useState(true);
 
   const accessToken = import.meta.env.VITE_MAPBOX_TOKEN;
   const initialCenter = [-60.17795, -6.82434];
@@ -50,13 +49,6 @@ function App() {
       style: "mapbox://styles/mapbox/satellite-v9",
       attributionControl: false,
     });
-    mapRefD.current = new mapboxgl.Map({
-      container: mapContainerRefD.current,
-      center: initialCenter,
-      zoom: initialZoom,
-      style: "mapbox://styles/mapbox/satellite-v9",
-      attributionControl: false,
-    });
 
     const setupMaps = () => {
       console.log("All maps loaded - setting up to sync");
@@ -64,7 +56,7 @@ function App() {
         mapRefA.current,
         mapRefB.current,
         mapRefC.current,
-        mapRefD.current,
+        /* mapRefD.current,*/
       );
     };
 
@@ -179,44 +171,9 @@ function App() {
       checkAllMapsLoaded();
     });
 
-    mapRefD.current.on("load", () => {
-      // Source: GeoJSON Placeholder D
-      mapRefD.current.addSource("level3-text-source", {
-        type: "geojson",
-        data: {
-          type: "Feature",
-          geometry: {
-            type: "Point",
-            coordinates: initialCenter,
-          },
-          properties: {
-            text: "LEVEL 3 - OVERVIEW",
-          },
-        },
-      });
-
-      // Layer: placeholder C
-      mapRefD.current.addLayer({
-        id: "level3-text-layer",
-        type: "symbol",
-        source: "level3-text-source",
-        layout: {
-          "text-field": ["get", "text"],
-          "text-size": 24,
-          visibility: "visible", // initial visible
-        },
-        paint: {
-          "text-color": "#ffffff",
-        },
-      });
-
-      checkAllMapsLoaded();
-    });
-
     mapRefA.current.on("load", checkAllMapsLoaded);
     mapRefB.current.on("load", checkAllMapsLoaded);
     mapRefC.current.on("load", checkAllMapsLoaded);
-    mapRefD.current.on("load", checkAllMapsLoaded);
 
     mapRefA.current.on("move", () => {
       const mapZoom = mapRefA.current.getZoom();
@@ -228,23 +185,15 @@ function App() {
       mapRefA.current.remove();
       mapRefB.current.remove();
       mapRefC.current.remove();
-      mapRefD.current.remove();
     };
   }, []);
 
   useEffect(() => {
-    if (
-      !mapRefA.current ||
-      !mapRefB.current ||
-      !mapRefC.current ||
-      !mapRefD.current
-    )
-      return;
+    if (!mapRefA.current || !mapRefB.current || !mapRefC.current) return;
     if (
       !mapRefA.current.isStyleLoaded() ||
       !mapRefB.current.isStyleLoaded() ||
-      !mapRefC.current.isStyleLoaded() ||
-      !mapRefD.current.isStyleLoaded()
+      !mapRefC.current.isStyleLoaded()
     )
       return;
 
@@ -264,11 +213,6 @@ function App() {
         activeLayer: activeLayerC,
         layerId: "level2-text-layer",
       },
-      {
-        mapRef: mapRefD,
-        activeLayer: activeLayerD,
-        layerId: "level3-text-layer",
-      },
     ];
 
     mapConfigs.forEach((config) => {
@@ -286,7 +230,7 @@ function App() {
         );
       }
     });
-  }, [activeLayerA, activeLayerB, activeLayerC, activeLayerD]);
+  }, [activeLayerA, activeLayerB, activeLayerC]);
 
   return (
     <>
@@ -294,14 +238,20 @@ function App() {
         <button onClick={() => setActiveLayerA(!activeLayerA)}>Level 0</button>
         <button onClick={() => setActiveLayerB(!activeLayerB)}>Level 1</button>
         <button onClick={() => setActiveLayerC(!activeLayerC)}>Level 2</button>
-        <button onClick={() => setActiveLayerD(!activeLayerD)}>Level3</button>
       </div>
       <div id="zoom-bar"> Zoom:{zoom.toFixed(2)}</div>
-      <div className="map-container">
+      <div
+        className="map-container"
+        style={{
+          display: "flex",
+          flex: 1,
+          position: "relative",
+          height: "100%",
+        }}
+      >
         <div id="map-a" ref={mapContainerRefA}></div>
         <div id="map-b" ref={mapContainerRefB}></div>
         <div id="map-c" ref={mapContainerRefC}></div>
-        <div id="map-d" ref={mapContainerRefD}></div>
       </div>
     </>
   );
